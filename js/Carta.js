9fc75fc650ec28a273fc6549c8b2aaf85384ea80
DiFranchescoo <adolfodfco@hotmail.com>
2023-01-17 23:06:56 +0100

    Juego
    .

---
+++ js/Carta.js
@@ -0,0 +1,25 @@
+class Carta{
+    reto
+    color
+
+    constructor(reto, color) {
+        this.reto = reto;
+        this.color = color;
+    }
+
+    get reto() {
+        return this.reto;
+    }
+
+    set reto(value) {
+        this.reto = value;
+    }
+
+    get color() {
+        return this.color;
+    }
+
+    set color(value) {
+        this.color = value;
+    }
+}
